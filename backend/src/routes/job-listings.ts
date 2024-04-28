a5d138f11e7fceb10fb8a8f4158d31b1fffe7f7e
Klaudia <klaudia.mr97@gmail.com>
2024-04-28 21:03:40 +0100

    Updated job-listing router

--- backend/src/routes/job-listings.ts
+++ backend/src/routes/job-listings.ts
@@ -1,21 +1,10 @@
 import express, { Request, Response } from "express";
-import multer from "multer";
-import cloudinary from "cloudinary";
 import JobListing, { JobListingType } from "../models/joblisting";
 import verifyToken from "../middleware/auth";
 import { body } from "express-validator";
 
 const router = express.Router();
 
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, //5MB
-  },
-});
-
-// api/job-listings
 router.post(
   "/",
   verifyToken,
@@ -36,19 +25,10 @@ router.post(
       .withMessage("Optional Skills are required"),
     body("description").notEmpty().withMessage("Description is required"),
   ],
-  upload.array("imageFiles", 1),
   async (req: Request, res: Response) => {
     try {
-      const imageFiles = req.files as Express.Multer.File[];
       const newListing: JobListingType = req.body;
 
-      const uploadPromises = imageFiles.map(async (image) => {
-        const b64 = Buffer.from(image.buffer).toString("base64");
-        let dataURI = "dat:" + image.mimetype + ";base64," + b64;
-        const res = await cloudinary.v2.uploader.upload(dataURI);
-        return res.url;
-      });
-      const imageUrls = await Promise.all(uploadPromises);
       newListing.userId = req.userId;
 
       const listing = new JobListing(newListing);
